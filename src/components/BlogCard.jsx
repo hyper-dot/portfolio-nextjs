4675ed141e5d5b884f3f43b3f994e66af6e02247
Roshan Paudel <rozanpoudel@gmail.com>
2023-07-29 12:43:47 +0545

    blog card edited for dynamic post fetching

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import readableDate from '@/utils/readableDate';
 
 const BlogCard = ({ blog }) => {
   return (
     <div className='border max-w-2xl border-gray-400 rounded-md p-4 flex flex-col md:flex-row'>
       <img
-        src={blog.imageUrl}
+        src={blog.img}
         alt={blog.title}
-        className='w-full md:w-40 h-30 object-cover rounded-md mr-4'
+        className='w-full md:w-40 h-40 md:h-30 object-cover rounded-md mr-4'
       />
       <div>
         <h2 className='text-md mt-4 md:mt-0 font-semibold mb-2'>
           {blog.title}
         </h2>
         <p className='text-sm mb-2'>
-          {blog.date} by {blog.author}
+          {readableDate(blog.createdAt)} by Roshan Paudel
         </p>
-        <p className='mb-4'>{blog.content}</p>
+        <p className='mb-4'>{blog.desc}</p>
         <Link
-          href={blog.link}
+          href={`/blogs/${blog.slug}`}
           rel='noopener noreferrer'
           className='inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded'
         >
