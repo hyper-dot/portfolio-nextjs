8721872be01d8037c095617bf8bfa253d52076d9
Roshan Paudel <rozanpoudel@gmail.com>
2023-07-29 15:49:53 +0545

    removed blog card

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import BlogCard from '@/components/BlogCard';
+import Link from 'next/link';
 import { getAllPosts } from '@/utils/post';
+import readableDate from '@/utils/readableDate';
 
 const BlogsShowcasePage = async () => {
   const data = await getAllPosts();
   return (
     <div className='p-10 pt-28 min-h-screen flex flex-wrap items-start justify-center'>
-      <div className='max-w-5xl mx-auto'>
+      <div className='max-w-2xl mx-auto'>
         <h1 className='text-3xl font-semibold mb-8'>All Blogs </h1>
         <div className='flex flex-col gap-8'>
-          {data.map((d) => (
-            <BlogCard key={d._id} blog={d} />
+          {data.map((post) => (
+            <Link
+              href={`blogs/${post.slug}`}
+              className='border border-gray-400 transition px-4  rounded-md '
+              key={post._id}
+            >
+              <small className=''>{readableDate(post.createdAt)}</small>
+              <h2 className='text-xl font-semibold mb-2'>{post.title}</h2>
+              <p className='mb-4'>{post.desc}</p>
+            </Link>
           ))}
         </div>
       </div>
