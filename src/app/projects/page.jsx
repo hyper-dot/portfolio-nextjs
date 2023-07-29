d3c0a05d79c10850a3bfb962dff72e0ac3d0401c
Roshan Paudel <rozanpoudel@gmail.com>
2023-07-29 10:37:16 +0545

    project frontend done

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -45,7 +45,9 @@ const ShowcasePage = () => {
   return (
     <div className='p-10 pt-28 min-h-screen flex flex-wrap items-start justify-center'>
       <div className='max-w-5xl mx-auto'>
-        <h1 className='text-3xl font-semibold mb-8'>My Projects</h1>
+        <h1 className='text-3xl font-semibold mb-8'>
+          🚀 Projects I have worked on !
+        </h1>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
           {projects.map((project) => (
             <ProjectCard key={project.id} project={project} />
