e43d483dc2d216eb3b1db9b73759c2a50a91244b
Roshan Paudel <rozanpoudel@gmail.com>
2023-07-29 19:46:27 +0545

    editing and deleting post

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,38 +1,17 @@
-import Link from 'next/link';
 import React from 'react';
-import { getAllPosts } from '@/utils/post';
-import { BsPencilSquare } from 'react-icons/bs';
-import { RiDeleteBinFill } from 'react-icons/ri';
+import Link from 'next/link';
 
-const page = async () => {
-  const data = await getAllPosts();
+const page = () => {
   return (
-    <div className='pt-10 min-h-screen flex justify-center'>
-      <h1>ALL Posts</h1>
-      <ul className='mt-10'>
-        {data.map((post) => (
-          <li className='list-disc mt-2'>
-            {post.title}
-            <Link
-              className='ml-4 text-xl inline-block hover:text-blue-600'
-              href={`admin/posts/edit?slug=${post.slug}`}
-            >
-              <BsPencilSquare />
-            </Link>
-            <Link
-              className='text-xl ml-4 inline-block hover:text-red-600'
-              href={`/posts`}
-            >
-              <RiDeleteBinFill />
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className='height-screen flex flex-col justify-center items-center gap-4'>
+      <Link className='px-10 py-2 bg-blue-500 rounded-md ' href='/admin/posts'>
+        See All Post
+      </Link>
       <Link
-        className='text-blue-500 underline underline-offset-4'
-        href='/admin/posts/new'
+        className='px-10 py-2 bg-blue-500 rounded-md '
+        href='/admin/projects'
       >
-        Create New Post
+        See All Projects
       </Link>
     </div>
   );
--- src/app/admin/posts/page.jsx
+++ src/app/admin/posts/page.jsx
@@ -1,9 +1,60 @@
-import React from 'react';
+'use client';
+import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
+import { BsPencilSquare } from 'react-icons/bs';
+import { RiDeleteBinFill } from 'react-icons/ri';
+import useSWR from 'swr';
+import axios from 'axios';
 
 const page = () => {
+  //NEW WAY TO FETCH DATA
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, mutate, error, isLoading } = useSWR(`/api/posts`, fetcher);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) {
+    console.log(error);
+    return <div>Error</div>;
+  }
+
+  const handleDelete = async (slug) => {
+    try {
+      await axios.delete(`/api/posts/${slug}`);
+      mutate();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div>
-      <h1>Admin list all post</h1>
+    <div className='pt-10 min-h-screen flex justify-center'>
+      <h1>ALL Posts</h1>
+      <ul className='mt-10'>
+        {data.map((post) => (
+          <li className='list-disc mt-2' key={post._id}>
+            {post.title}
+            <Link
+              className='ml-4 text-xl inline-block hover:text-blue-600'
+              href={`/admin/posts/edit?slug=${post.slug}`}
+            >
+              <BsPencilSquare />
+            </Link>
+            <button
+              className='text-xl ml-4 inline-block hover:text-red-600'
+              onClick={() => handleDelete(post.slug)}
+            >
+              <RiDeleteBinFill />
+            </button>
+          </li>
+        ))}
+      </ul>
+      <Link
+        className='text-blue-500 underline underline-offset-4'
+        href='/admin/posts/new'
+      >
+        Create New Post
+      </Link>
     </div>
   );
 };
