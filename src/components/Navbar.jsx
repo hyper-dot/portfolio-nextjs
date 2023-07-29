d5dff1e1cd05a40b56478ec445f134a936afb092
Roshan Paudel <rozanpoudel@gmail.com>
2023-07-29 21:22:14 +0545

    optimized css and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,16 +7,16 @@ const Navbar = () => {
     <nav className='p-4'>
       <div className='flex items-center justify-center'>
         <div className='flex space-x-4'>
-          <Link href='/' className='font-semibold hover:text-gray-600'>
+          <Link href='/' className='font-semibold hover:text-blue-500'>
             Home
           </Link>
-          <Link href='/about' className='font-semibold hover:text-gray-600'>
+          <Link href='/about' className='font-semibold hover:text-blue-500'>
             About
           </Link>
-          <Link href='/projects' className='font-semibold hover:text-gray-600'>
+          <Link href='/projects' className='font-semibold hover:text-blue-500'>
             Projects
           </Link>
-          <Link href='/contact' className='font-semibold hover:text-gray-600'>
+          <Link href='/contact' className='font-semibold hover:text-blue-500'>
             Contact
           </Link>
           <div className='cursor-pointer'>
