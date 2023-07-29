45f752bf6090b5ba9f50a797e058333630c2d340
Roshan Paudel <rozanpoudel@gmail.com>
2023-07-29 15:49:35 +0545

    removed blog image

--- src/app/blogs/[slug]/page.jsx
+++ src/app/blogs/[slug]/page.jsx
@@ -9,12 +9,8 @@ const page = async ({ params }) => {
   return (
     <div className='p-10 pt-28 min-h-screen flex flex-wrap items-start justify-center'>
       <div className='max-w-3xl'>
-        <img
-          src={blog.img}
-          className='w-full h-80  object-cover rounded-md mb-4'
-        />
         <p className='text-gray-400 text-sm mb-2'>
-          {readableDate(blog.createdAt)} by Roshan Paudel
+          ✍️ {readableDate(blog.createdAt)} by Roshan Paudel
         </p>
         <h1 className='text-3xl font-semibold mb-4'>{blog.title}</h1>
         <div className='content mb-4' dangerouslySetInnerHTML={markup}></div>
