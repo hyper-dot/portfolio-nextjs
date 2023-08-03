88f533ec29088e837b971b397e4f27616e701352
Roshan Paudel <rozanpoudel@gmail.com>
2023-08-03 13:52:56 +0545

    removed warning

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,35 +5,27 @@ import { useState } from 'react';
 
 const Navbar = () => {
   return (
-    <>
-      <div className='bg-orange-200 text-black py-4 flex justify-center flex-wrap'>
-        ⚠️ Warning ! This site is under construction !!
-      </div>
-      <nav className='pt-4'>
-        <div className='flex items-center justify-center'>
-          <div className='flex space-x-4'>
-            <Link href='/' className='font-semibold hover:text-blue-500'>
-              Home
-            </Link>
-            <Link href='/about' className='font-semibold hover:text-blue-500'>
-              About
-            </Link>
-            <Link
-              href='/projects'
-              className='font-semibold hover:text-blue-500'
-            >
-              Projects
-            </Link>
-            <Link href='/contact' className='font-semibold hover:text-blue-500'>
-              Contact
-            </Link>
-            <div className='cursor-pointer'>
-              <ThemeToggle />
-            </div>
+    <nav className='pt-4'>
+      <div className='flex items-center justify-center'>
+        <div className='flex space-x-4'>
+          <Link href='/' className='font-semibold hover:text-blue-500'>
+            Home
+          </Link>
+          <Link href='/about' className='font-semibold hover:text-blue-500'>
+            About
+          </Link>
+          <Link href='/projects' className='font-semibold hover:text-blue-500'>
+            Projects
+          </Link>
+          <Link href='/contact' className='font-semibold hover:text-blue-500'>
+            Contact
+          </Link>
+          <div className='cursor-pointer'>
+            <ThemeToggle />
           </div>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
