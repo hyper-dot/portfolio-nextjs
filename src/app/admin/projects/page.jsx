2be26bd6ec439827b5531c0faddb158b6e8a946d
Roshan Paudel <rozanpoudel@gmail.com>
2023-08-03 07:04:09 +0545

    fixed errors

--- src/app/admin/projects/page.jsx
+++ src/app/admin/projects/page.jsx
@@ -6,10 +6,8 @@ import { RiDeleteBinFill } from 'react-icons/ri';
 import useSWR from 'swr';
 import axios from 'axios';
 import Spinner from '@/components/Spinner';
-=======
 import React from 'react';
 import Link from 'next/link';
-
 
 const page = () => {
   //NEW WAY TO FETCH DATA
@@ -38,40 +36,40 @@ const page = () => {
   };
 
   return (
-
-    <div className='pt-10 min-h-screen flex justify-center'>
-      <h1>ALL Posts</h1>
-      <ul className='mt-10 max-w-2xl'>
-        {data.map((project) => (
-          <li className='list-disc mt-2' key={project._id}>
-            {project.title}
-            <Link
-              className='ml-4 text-xl inline-block hover:text-blue-600'
-              href={`/admin/posts/edit`}
-            >
-              <BsPencilSquare />
-            </Link>
-            <button
-              className='text-xl ml-4 inline-block hover:text-red-600'
-              onClick={() => handleDelete()}
-            >
-              <RiDeleteBinFill />
-            </button>
-          </li>
-        ))}
-      </ul>
-      <Link
-        className='text-blue-500 underline underline-offset-4'
-        href='/admin/projects/new'
-      >
-        Create New Project
-      </Link>
-=======
-    <div>
-      <h2>All Projects</h2>
-      <Link href='/admin/projects/new'>Add New Project</Link>
-
-    </div>
+    <>
+      <div className='pt-10 min-h-screen flex justify-center'>
+        <h1>ALL Posts</h1>
+        <ul className='mt-10 max-w-2xl'>
+          {data.map((project) => (
+            <li className='list-disc mt-2' key={project._id}>
+              {project.title}
+              <Link
+                className='ml-4 text-xl inline-block hover:text-blue-600'
+                href={`/admin/posts/edit`}
+              >
+                <BsPencilSquare />
+              </Link>
+              <button
+                className='text-xl ml-4 inline-block hover:text-red-600'
+                onClick={() => handleDelete()}
+              >
+                <RiDeleteBinFill />
+              </button>
+            </li>
+          ))}
+        </ul>
+        <Link
+          className='text-blue-500 underline underline-offset-4'
+          href='/admin/projects/new'
+        >
+          Create New Project
+        </Link>
+      </div>
+      <div>
+        <h2>All Projects</h2>
+        <Link href='/admin/projects/new'>Add New Project</Link>
+      </div>
+    </>
   );
 };
 
