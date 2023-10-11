7a88ddd307c12853602c251f68afb4a9aca7b7a8
Roshan Paudel <rozanpoudel@gmail.com>
2023-10-11 17:20:43 +0545

    fix: projects admin page layout

--- src/app/admin/posts/edit/page.jsx
+++ src/app/admin/posts/edit/page.jsx
@@ -1,21 +1,21 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import axios from 'axios';
-import Spinner from '@/components/admin/Spinner';
+"use client";
+import React, { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import axios from "axios";
+import Spinner from "@/components/admin/Spinner";
 
 const Page = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const slug = searchParams.get('slug');
+  const slug = searchParams.get("slug");
 
   // Data fetching
   const [loading, setLoading] = useState(true);
   const [formData, setFormData] = useState({
-    title: '',
-    desc: '',
-    keywords: '',
-    markdown: '',
+    title: "",
+    desc: "",
+    keywords: "",
+    markdown: "",
   });
 
   useEffect(() => {
@@ -29,8 +29,8 @@ const Page = () => {
 
   if (!formData) {
     return (
-      <div className='h-screen'>
-        <div className='h-3/4 flex justify-center items-center text-red-500 text-3xl font-bold'>
+      <div className="h-screen">
+        <div className="h-3/4 flex justify-center items-center text-red-500 text-3xl font-bold">
           Are you out of your mind ? There is no post having that slug !!!
         </div>
       </div>
@@ -60,73 +60,73 @@ const Page = () => {
   };
 
   return (
-    <div className='col-span-3 w-full max-w-4xl'>
-      <h1 className='text-center font-bold text-2xl'>Edit Blog Post</h1>
-      <div className='p-10 mx-auto'>
+    <div className="col-span-3 w-full max-w-4xl">
+      <h1 className="text-center font-bold text-2xl">Edit Blog Post</h1>
+      <div className="p-10 mx-auto">
         <form onSubmit={handleSubmit}>
-          <div className='mb-4'>
-            <label htmlFor='title' className='block font-semibold mb-2'>
+          <div className="mb-4">
+            <label htmlFor="title" className="block font-semibold mb-2">
               Title
             </label>
             <input
-              type='text'
+              type="text"
               value={formData.title}
               onChange={handleChange}
-              id='title'
-              name='title'
-              className='bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500'
+              id="title"
+              name="title"
+              className="bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500"
               required
             />
           </div>
-          <div className='mb-4'>
-            <label htmlFor='description' className='block font-semibold mb-2'>
+          <div className="mb-4">
+            <label htmlFor="description" className="block font-semibold mb-2">
               Description
             </label>
             <input
-              type='text'
-              id='description'
+              type="text"
+              id="description"
               value={formData.desc}
-              name='desc'
+              name="desc"
               onChange={handleChange}
-              className='bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500'
+              className="bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500"
               required
             />
           </div>
 
-          <div className='mb-4'>
-            <label htmlFor='keywords' className='block font-semibold mb-2'>
+          <div className="mb-4">
+            <label htmlFor="keywords" className="block font-semibold mb-2">
               Keywords
             </label>
             <input
-              type='text'
+              type="text"
               value={formData.keywords}
               onChange={handleChange}
-              id='keywords'
-              name='keywords'
-              className='bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500'
+              id="keywords"
+              name="keywords"
+              className="bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500"
               required
             />
           </div>
 
-          <div className='mb-4'>
-            <label htmlFor='content' className='block font-semibold mb-2'>
+          <div className="mb-4">
+            <label htmlFor="content" className="block font-semibold mb-2">
               Content
             </label>
             <textarea
-              style={{ fontFamily: 'monospace' }}
-              id='content'
+              style={{ fontFamily: "monospace" }}
+              id="content"
               rows={20}
-              name='markdown'
+              name="markdown"
               value={formData.markdown}
               onChange={handleChange}
-              className='bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500'
+              className="bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500"
               required
             />
           </div>
-          <div className='text-green-500 mb-4'></div>
+          <div className="text-green-500 mb-4"></div>
           <button
-            type='submit'
-            className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded'
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
           >
             Save Edit
           </button>
--- src/app/admin/projects/edit/page.jsx
+++ src/app/admin/projects/edit/page.jsx
@@ -1,21 +1,21 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import axios from 'axios';
-import Spinner from '@/components/Spinner';
+"use client";
+import React, { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import axios from "axios";
+import Spinner from "@/components/Spinner";
 
 const Page = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const id = searchParams.get('id');
+  const id = searchParams.get("id");
 
   // Data fetching
   const [loading, setLoading] = useState(true);
   const [formData, setFormData] = useState({
-    title: '',
-    desc: '',
-    liveUrl: '',
-    githubUrl: '',
+    title: "",
+    desc: "",
+    liveUrl: "",
+    githubUrl: "",
   });
 
   useEffect(() => {
@@ -29,8 +29,8 @@ const Page = () => {
 
   if (!formData) {
     return (
-      <div className='height-screen'>
-        <div className='h-3/4 flex justify-center items-center text-red-500 text-3xl font-bold'>
+      <div className="height-screen">
+        <div className="h-3/4 flex justify-center items-center text-red-500 text-3xl font-bold">
           No Project Found !!!
         </div>
       </div>
@@ -69,77 +69,86 @@ const Page = () => {
   };
 
   return (
-    <div>
-      <h1>
-        <div className='min-h-screen p-10 max-w-4xl mx-auto'>
-          <form onSubmit={handleSubmit}>
-            <div className='mb-4'>
-              <label htmlFor='title' className='block font-semibold mb-2'>
-                Title
-              </label>
-              <input
-                type='text'
-                value={formData.title}
-                onChange={handleChange}
-                id='title'
-                name='title'
-                className='bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500'
-                required
-              />
-            </div>
-            <div className='mb-4'>
-              <label htmlFor='description' className='block font-semibold mb-2'>
-                Description
-              </label>
-              <input
-                type='text'
-                id='description'
-                value={formData.desc}
-                name='desc'
-                onChange={handleChange}
-                className='bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500'
-                required
-              />
-            </div>
-
-            <div className='mb-4'>
-              <label htmlFor='liveUrl' className='block font-semibold mb-2'>
-                Live Url
-              </label>
-              <input
-                type='text'
-                id='liveUrl'
-                value={formData.liveUrl}
-                name='liveUrl'
-                onChange={handleChange}
-                className='bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500'
-                required
-              />
-            </div>
-            <div className='mb-4'>
-              <label htmlFor='githubUrl' className='block font-semibold mb-2'>
-                Github Url
-              </label>
-              <input
-                type='text'
-                id='githubUrl'
-                value={formData.githubUrl}
-                name='githubUrl'
-                onChange={handleChange}
-                className='bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500'
-                required
-              />
-            </div>
-
-            <button
-              type='submit'
-              className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded'
-            >
-              Save Edit
-            </button>
-          </form>
+    <div className="col-span-3 w-full max-w-4xl p-4">
+      <h1 className="text-center font-bold text-2xl">Edit Project</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label htmlFor="title" className="block font-semibold mb-2">
+            Title
+          </label>
+          <input
+            type="text"
+            value={formData.title}
+            onChange={handleChange}
+            id="title"
+            name="title"
+            className="bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500"
+            required
+          />
         </div>
-      </h1>
+        <div className="mb-4">
+          <label htmlFor="description" className="block font-semibold mb-2">
+            Description
+          </label>
+          {/*
+              <input
+                type="text"
+                id="description"
+                value={formData.desc}
+                name="desc"
+                onChange={handleChange}
+                className="bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500"
+                required
+              />
+            */}
+          <textarea
+            className="bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500"
+            name="desc"
+            id="description"
+            cols="30"
+            rows="5"
+            onChange={handleChange}
+            required
+            value={formData.desc}
+          ></textarea>
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="liveUrl" className="block font-semibold mb-2">
+            Live Url
+          </label>
+          <input
+            type="text"
+            id="liveUrl"
+            value={formData.liveUrl}
+            name="liveUrl"
+            onChange={handleChange}
+            className="bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="githubUrl" className="block font-semibold mb-2">
+            Github Url
+          </label>
+          <input
+            type="text"
+            id="githubUrl"
+            value={formData.githubUrl}
+            name="githubUrl"
+            onChange={handleChange}
+            className="bg-transparent w-full px-4 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring focus:border-blue-500"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+        >
+          Save Edit
+        </button>
+      </form>
     </div>
   );
 };
