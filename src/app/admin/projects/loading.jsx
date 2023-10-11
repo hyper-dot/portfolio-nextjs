adaeaf0da5a882a3f3c218a9c81c1c96a73c2e37
Roshan Paudel <rozanpoudel@gmail.com>
2023-10-11 17:28:27 +0545

    loading ui fixed admin projects page

--- src/app/admin/projects/loading.jsx
+++ src/app/admin/projects/loading.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Spinner from '@/components/admin/Spinner';
+import React from "react";
+import Spinner from "@/components/admin/Spinner";
 
 const loading = () => {
   return (
-    <div className='col-span-3 h-full flex justify-center items-center'>
+    <div className="col-span-3 w-full h-full flex justify-center items-center">
       <Spinner />
     </div>
   );
